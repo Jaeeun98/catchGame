fd8d9a600549c7c807c2ca9410875422201b3414
Jaeeun98 <wodms4525@naver.com>
2021-08-23 21:15:45 +0900

    Title : gameStop Add
    gameStop Add

--- js/downGame.js
+++ js/downGame.js
@@ -3,9 +3,7 @@ const list = document.querySelectorAll('.item')[0].children
 const meat = document.querySelectorAll('.meat');
 const startBtn = document.querySelector('.start');
 const stopBtn = document.querySelector('.stop');
-
-
-window.onload = setPositionX;
+let startEnd = '';
 
 function setPositionX(){
     for(let i=0; i<list.length; i++){
@@ -15,13 +13,15 @@ function setPositionX(){
 }
 
 function gameStart(){
+    setPositionX();
+
     let i=0;
     const moveAnimate = setInterval(() => {
         const sectionX = document.querySelector('section').getBoundingClientRect().left;
         const listX = list[i].getBoundingClientRect().left;
         const x = listX - sectionX;
-        
-        list[i].style.zIndex = 0;
+
+        //animate 부여
         list[i].animate([
             { transform :`translate(${x}px, 500px)`}
         ], {
@@ -29,17 +29,32 @@ function gameStart(){
             iterations:Infinity,
         })
         
-        i++;
-        i == 21 && clearInterval(moveAnimate)
+        //gameStop이면, animation 제거 
+        if(startEnd === 'stop'){
+            list[i].getAnimations()[0].cancel();
+            i = list.length;
+
+            startEnd = 'start';
+        } else {
+            list[i].style.zIndex = 0;
+            i++;
+        }
+        
+        i == list.length && clearInterval(moveAnimate);
     }, 1000)
 }
 
 function gameStop(){
-    const getAni = list[0].getAnimations();
-    console.log(getAni);
-    getAni.pause();
-    
+    for(let i=0; i < list.length; i++){
+        const getAni = list[i].getAnimations();
+        getAni.length != 0 && getAni[0].cancel();
+        list[i].style.zIndex = -1;
+    }
+
+   startEnd = 'stop'
 }
+
+
 
 
 startBtn.addEventListener('click', gameStart);
